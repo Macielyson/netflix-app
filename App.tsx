413e108ba92a1a85f1a5f158803cd75ae849d44d
Macielyson Nascimento <macielyson.s@gmail.com>
2023-10-09 18:56:46 -0300

    primeiro commit

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Text } from "react-native";
+
+const App = () => {
+  return (
+    <>
+      <Text>
+        Hello Word
+      </Text>
+    </>
+
+  );
+}
+
+export default App;
